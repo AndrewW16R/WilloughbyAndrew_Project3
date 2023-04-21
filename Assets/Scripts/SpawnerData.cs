b5f7bbf38fde3d536006fbded643d11454797343
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-21 15:03:23 -0500

    Added _objectToSpawn field to SpawnerData

--- Assets/Scripts/SpawnerData.cs
+++ Assets/Scripts/SpawnerData.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class SpawnerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Settings")]
+    [SerializeField]
+    private GameObject _objectToSpawn;
 }
