865bffc0faeea1f912141de62da04feb51112f32
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-21 15:27:39 -0500

    Changed SpawnerData from monoBehavior to ScriptableObject

--- Assets/Scripts/SpawnerData.cs
+++ Assets/Scripts/SpawnerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerData : MonoBehaviour
+public class SpawnerData : ScriptableObject
 {
     [Header("Spawner Settings")]
     [SerializeField]
