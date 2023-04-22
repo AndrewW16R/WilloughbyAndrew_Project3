00ed461684661d82d7813ea439999d1e1132b30b
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-22 18:03:26 -0500

    Setup code for SpawnerForceShutoffTest

--- Assets/Scripts/SpawnerForceShutoffTest.cs
+++ Assets/Scripts/SpawnerForceShutoffTest.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpawnerForceShutoffTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent OnQPress;
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
+       if (Input.GetKeyDown(KeyCode.Q))
+        {
+            OnQPress.Invoke();
+        }
     }
 }
