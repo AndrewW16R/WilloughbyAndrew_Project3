f997bd806aef384f97e81e3a8876d18bc4dc8327
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-22 15:17:16 -0500

    Added AssetMenu to EndlessSpawnerData

--- Assets/Scripts/EndlessSpawnerData.cs
+++ Assets/Scripts/EndlessSpawnerData.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EndlessSpawnerData_", menuName = "SpawnData/EndlessSpawner")]
+
 public class EndlessSpawnerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
