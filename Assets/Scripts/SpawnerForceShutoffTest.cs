e6d1666efd49f384bee33ec51074569178539e49
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-22 18:05:21 -0500

    Added _enableThisScript bool field to SpawnerForceShutoffTest

--- Assets/Scripts/SpawnerForceShutoffTest.cs
+++ Assets/Scripts/SpawnerForceShutoffTest.cs
@@ -6,13 +6,14 @@ using UnityEngine.Events;
 public class SpawnerForceShutoffTest : MonoBehaviour
 {
     public UnityEvent OnQPress;
+    [SerializeField] private bool _enableThisScript = false;
 
     // Update is called once per frame
     void Update()
     {
        
 
-       if (Input.GetKeyDown(KeyCode.Q))
+       if (Input.GetKeyDown(KeyCode.Q) && _enableThisScript == true)
         {
             OnQPress.Invoke();
         }
