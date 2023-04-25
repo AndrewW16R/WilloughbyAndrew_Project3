2024ed6bb1270055ebfbff568d7d4a4173898455
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-25 14:54:01 -0500

    Add MoveTowardsPlayer method with functionality in EnemyMovement script

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] GameObject _playerObject;
+    private NavMeshAgent _agent;
+    Transform _targetPosition;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _targetPosition = _playerObject.transform;
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsPlayer();
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        _agent.destination = _targetPosition.position;
     }
 }
