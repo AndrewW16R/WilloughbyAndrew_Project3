5d1727d843c0ddac97c05a4c6daf7de5dab1c41f
Andrew W <andrewwilloughbyr16@gmail.com>
2023-04-25 14:42:09 -0500

    Created Player Capsule Script with Move method

---
+++ Assets/Scripts/PlayerCapsule.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCapsule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-Vector3.right * Time.deltaTime);
+        }
+    }
+}
+
+
